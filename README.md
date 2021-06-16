d95a78e8955bf8f4c349eb8c64c290a5c9086768
Rafflesiaceae <rafflesiaceae.plant@gmail.com>
2021-06-16 23:35:03 +0200

    hello world

---
+++ README.md
@@ -0,0 +1,10 @@
+# About
+erm... it encodes/decodes stdin to groovy single-quote string literals, because
+the menagerie of groovy string literals are all maximally unintuitive and pretty
+much a complete mess
+
+because probably most groovy users at this point are or will be people dealing
+with jenkins as gradle is moving to kotlin, might as well call it jenkcode and
+not groovcode, it's also just objectively a better name
+
+have a good day/night
---
+++ test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -eo pipefail
+
+# fAnCY AsCiI CoLoRs
+cBgreen=$(2>/dev/null  tput setab 2) || true
+cbold=$(2>/dev/null    tput bold)    || true
+cgreen=$(2>/dev/null   tput setaf 2) || true
+cmagenta=$(2>/dev/null tput setaf 5) || true
+cred=$(2>/dev/null     tput setaf 1) || true
+creset=$(2>/dev/null   tput sgr0)    || true
+
+failed=0
+
+tmpfile=$(mktemp)
+cleanup_tmpfile() {
+    rm "$tmpfile"
+}; trap 'cleanup_tmpfile' EXIT
+
+# just pretty print stuff
+maxpathlen=0
+for path in ./test_jenkcode_*; do [[ ${#path} -gt $maxpathlen ]] && maxpathlen=${#path}; done
+for path in ./test_jenkcode_*; do # actually run tests
+    printf "%s[TEST]%s %-${maxpathlen}s " "$cmagenta" "$creset" "$path"
+    # encode then decode and compare if we got the input back out
+    if cat "$path" | ./jenkode -e | ./jenkode -d | diff --color -u "$path" -; then
+        # run encoded through groovy's `print()` and compare if we got the input back out
+        printf "print(" > "$tmpfile"
+        cat "$path" | ./jenkode -e >> "$tmpfile"
+        printf ")" >> "$tmpfile"
+        if groovy "$tmpfile" 2>/dev/null | diff --color -u "$path" -; then
+            printf "%s  %s\n" "$cBgreen" "$creset"
+        else
+            ((failed++)) || true
+        fi
+    else
+        ((failed++)) || true
+    fi
+done
+if [[ $failed -gt 0 ]]; then
+    printf "\n%s%s%s tests %sfailed%s 🙁\n" "$cbold" "$failed" "$creset" \
+        "$cred" "$creset"
+else
+    printf "\nall tests %ssucceeded%s, have a nice day 😀\n" "$cgreen" "$creset"
+fi
